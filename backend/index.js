cb37db304d4723fabd97914a2af062d684e7cb8d
Krishnaveni mandula <krishnavenimandula123@gmail.com>
2024-08-23 00:57:03 +0100

    fixing cors issue

--- backend/index.js
+++ backend/index.js
@@ -10,25 +10,6 @@ const authRoutes = require("./routes/auth");
 
 connection();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
-    credentials: true, // If you need to include credentials like cookies in requests
-  })
-);
-
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.status(200).end();
-});
-
 //middleware
 
 app.use(express.json());
