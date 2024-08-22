af0875d161a35f06c3e29fc7aba963b6dd479596
Krishnaveni mandula <krishnavenimandula123@gmail.com>
2024-08-23 00:44:03 +0100

    fixing cors issue

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,19 @@ app.use(
   })
 );
 
+app.options("*", (req, res) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://flixxit-one.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(200).end();
+});
+
 //middleware
 
 app.use(express.json());
