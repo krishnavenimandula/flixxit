7a9324821a6d4b21ba7e8ccafdb0bfe5c0d7afe8
Krishnaveni mandula <krishnavenimandula123@gmail.com>
2024-08-23 08:35:17 +0100

    fixing cors issue

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const authRoutes = require("./routes/auth");
 
 app.use(
   cors({
-    origin: "https://flixxit-one.vercel.app", // Allow requests from this origin
+    origin: "*", // Allow requests from this origin
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true, // If you are sending cookies or authorization headers
